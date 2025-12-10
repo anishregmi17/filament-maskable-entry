23c43efcee3d101a0ac4eb3ff7bb5ff8f90c4b71
Anish Regmi <103883548+anishregmi17@users.noreply.github.com>
2025-12-10 15:25:16 +0545

    Add toggleable functionality to MaskableEntry component

--- src/Components/MaskableEntry.php
+++ src/Components/MaskableEntry.php
@@ -11,4 +11,19 @@ class MaskableEntry extends TextEntry
     use HasMaskableValueTrait;
 
     protected string $view = 'maskable-entry::components.maskable-entry';
+
+    
+    protected bool|Closure $isToggleable = true;
+
+    public function toggleable(bool|Closure $condition = true): static
+    {
+        $this->isToggleable = $condition;
+
+        return $this;
+    }
+
+    public function isToggleable(): bool
+    {
+        return $this->evaluate($this->isToggleable);
+    }
 }
